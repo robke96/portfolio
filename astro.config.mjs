6fe06d0e1c8cd6737fac38d34113e6a23beeb9a1
Robke96 <robertaszencius@gmail.com>
2024-12-30 17:48:37 +0200

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()]
+  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  adapter: netlify()
 });
